041bcb312623d4dc9baf117f54496c2b930c5ed5
crypte-1 <renukavutukuri1@gmail.com>
2022-05-19 21:07:31 +0530

    added token.sol

---
+++ Token.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.1;
+ 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract token is ERC20
+{
+    constructor() ERC20("name","symbl")
+    {
+        _mint (msg.sender,1000000 * (10**18));
+        
+    }
+
+
+}
